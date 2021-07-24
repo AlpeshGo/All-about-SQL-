793b7f0aebabb21dd71335b97d89e1f2fd042e43
Alpesh Gorde <80307439+AlpeshGo@users.noreply.github.com>
2021-07-24 09:25:54 +0530

    Update JOIN_Solution_Script.sql

--- Dataset and SQL for Data Science in Python/JOIN_Solution_Script.sql
+++ Dataset and SQL for Data Science in Python/JOIN_Solution_Script.sql
@@ -1,10 +1,11 @@
-
+# 1A Select the names and job start dates of all employees who work for the department number 5.
 --- Query1A ---
 select E.F_NAME,E.L_NAME, JH.START_DATE 
 	from EMPLOYEES as E 
 	INNER JOIN JOB_HISTORY as JH on E.EMP_ID=JH.EMPL_ID 
 	where E.DEP_ID ='5'
-;	
+;
+# 1B Select the names, job start dates, and job titles of all employees who work for the department number 5.
 --- Query1B ---	
 select E.F_NAME,E.L_NAME, JH.START_DATE, J.JOB_TITLE 
 	from EMPLOYEES as E 
@@ -12,34 +13,40 @@ select E.F_NAME,E.L_NAME, JH.START_DATE, J.JOB_TITLE
 	INNER JOIN JOBS as J on E.JOB_ID=J.JOB_IDENT
 	where E.DEP_ID ='5'
 ;
+#2A Perform a Left Outer Join on the EMPLOYEES and DEPARTMENT tables and select employee id, last name, department id and department name for all employees.
 --- Query 2A ---
 select E.EMP_ID,E.L_NAME,E.DEP_ID,D.DEP_NAME
 	from EMPLOYEES AS E 
 	LEFT OUTER JOIN DEPARTMENTS AS D ON E.DEP_ID=D.DEPT_ID_DEP
-;	
+;
+#2B Re-write the previous query but limit the result set to include only the rows for employees born before 1980.
 --- Query 2B ---
 select E.EMP_ID,E.L_NAME,E.DEP_ID,D.DEP_NAME
 	from EMPLOYEES AS E 
 	LEFT OUTER JOIN DEPARTMENTS AS D ON E.DEP_ID=D.DEPT_ID_DEP 
 	where YEAR(E.B_DATE) < 1980
 ;
+
 --- alt Query 2B ---
 select E.EMP_ID,E.L_NAME,E.DEP_ID,D.DEP_NAME
 	from EMPLOYEES AS E 
 	INNER JOIN DEPARTMENTS AS D ON E.DEP_ID=D.DEPT_ID_DEP 
 	where YEAR(E.B_DATE) < 1980
 ;
+#2C Re-write the previous query but have the result set include all the employees but department names for only the employees who were born before 1980.
 --- Query 2C ---
 select E.EMP_ID,E.L_NAME,E.DEP_ID,D.DEP_NAME
 	from EMPLOYEES AS E 
 	LEFT OUTER JOIN DEPARTMENTS AS D ON E.DEP_ID=D.DEPT_ID_DEP 
 	AND YEAR(E.B_DATE) < 1980
 ;
+#3A Perform a Full Join on the EMPLOYEES and DEPARTMENT tables and select the First name, Last name and Department name of all employees.
 --- Query 3A ---
 select E.F_NAME,E.L_NAME,D.DEP_NAME
 	from EMPLOYEES AS E 
 	FULL OUTER JOIN DEPARTMENTS AS D ON E.DEP_ID=D.DEPT_ID_DEP
 ;
+#3B Re-write the previous query but have the result set include all employee names but department id and department names only for male employees.
 --- Query 3B ---
 select E.F_NAME,E.L_NAME,D.DEPT_ID_DEP, D.DEP_NAME
 	from EMPLOYEES AS E 
